8811d6755e448e23da1c2cade7b85db93f4bee39
Jesse Clark <hello@developerjesse.com>
2020-11-18 18:26:24 +0200

    Move dependency to dev and remove unused variable.

--- src/app/state/reducers/ethrdid.test.ts
+++ src/app/state/reducers/ethrdid.test.ts
@@ -3,7 +3,6 @@ import { DIDDocument } from 'did-resolver'
 import ethrdidSlice, { EtherdidState, initialState, resolveDid } from './ethrdid'
 
 describe('ethrdid slice', () => {
-  const owner = '0x3dd03d7d6c3137f1eb7582ba5957b8a2e26f304a'
   const data: DIDDocument = {
     '@context': 'https://w3id.org/did/v1',
     id: '',
