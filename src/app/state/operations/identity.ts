c600be79ebe5857d5b063a0110cdaa56eaccc1ed
Jesse Clark <hello@developerjesse.com>
2021-01-14 12:27:25 +0200

    Update DataVault to use Encryption (#39)
    * Update DataVault to use Encryption
    
    - Use latest package of pinner service
    - Works on all networks, not just RSK Testnet
    
    This fixes the following issues:
    
    - Fixed #32 - Broken connections to the datavault are thrown and the client is not saved into context.
    - Fixed #30 - DataVault now only requests auth token once with the init getStorageInfo() command.
    - Fixed #23 - DataVault works on Ethereum and RSK Mainnet
    
    * Missed linebreak and package update.
    
    * Use v0 dv endpoint.

--- src/app/state/operations/identity.ts
+++ src/app/state/operations/identity.ts
@@ -1,12 +1,12 @@
 import { Dispatch } from 'react'
+
 import { getAccountAndNetwork } from '../../../ethrpc'
 import { rLogin } from '../../../features/rLogin'
 
 import { changeAccount, changeChainId } from '../reducers/identity'
 import { resolveDidDocument } from './ethrdid'
 import { getBalance, getTokenList } from './defi'
-import { createClient, getDataVaultContent, getStorageInformation } from './datavault'
-import { createDidFormat } from '../../../formatters'
+import { dataVaultStart } from './datavault'
 
 /**
  * Login into web3 provider via rLogin
@@ -25,11 +25,8 @@ export const login = (context: any) => (dispatch: Dispatch<any>) =>
       dispatch(getTokenList(provider, parseInt(chainId), address))
       dispatch(getBalance(provider, address))
 
-      const dataVaultClient = createClient(provider, address, chainId)
-      context.setDvClient(dataVaultClient)
-
-      dataVaultClient && dispatch(getDataVaultContent(dataVaultClient, createDidFormat(address, chainId)))
-      dataVaultClient && dispatch(getStorageInformation(dataVaultClient))
+      const callback = (dvClient: any, _error: any) => context.setDvClient(dvClient)
+      dispatch(dataVaultStart(provider, address, chainId, callback))
     })
   })
     .catch((err: string) => console.log('rLogin Error', err))
