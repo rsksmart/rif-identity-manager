0c5343dba7b1aabaa5c1e54ab7920de50950a485
Jesse Clark <hello@developerjesse.com>
2021-02-08 13:08:09 +0200

    Use the did-jwt version from RSK to sign the presentations. (#50)
    * Use the did-jwt version from RSK to sign the presentations.
    
    * Use link to verifier instead of textarea with DID.

--- src/features/credentials.ts
+++ src/features/credentials.ts
@@ -1,16 +1,21 @@
-import { Issuer, JwtPresentationPayload, createVerifiablePresentationJwt } from 'did-jwt-vc'
+import { createJWT } from 'jesse-did-jwt'
+import { fromRpcSig } from 'ethereumjs-util'
 import { getAccountAndNetwork } from '../ethrpc'
 import { createDidFormat } from '../formatters'
 
 export const createPresentation = (provider: any, jwt: string) => {
   return getAccountAndNetwork(provider).then(([address, chainId]) => {
     const did = createDidFormat(address, chainId)
-    const issuer: Issuer = {
-      did,
-      signer: (data: string) => provider.request({ method: 'personal_sign', params: [data, address] })
-    }
 
-    const vpPayload: JwtPresentationPayload = {
+    const signer = (data: string) => provider.request({ method: 'personal_sign', params: [data, address] })
+      .then(fromRpcSig)
+      .then(({ v, r, s }: { v: number, r: Buffer, s: Buffer }) => ({
+        r: r.toString('hex'),
+        s: s.toString('hex'),
+        recoveryParam: v
+      }))
+
+    const vpPayload = {
       vp: {
         '@context': ['https://www.w3.org/2018/credentials/v1'],
         type: ['VerifiablePresentation'],
@@ -19,6 +24,7 @@ export const createPresentation = (provider: any, jwt: string) => {
         exp: Math.floor(new Date().getTime() / 1000) + 3600
       }
     }
-    return createVerifiablePresentationJwt(vpPayload, issuer)
+
+    return createJWT(vpPayload, { alg: 'ES256K', issuer: did, signer })
   })
 }
